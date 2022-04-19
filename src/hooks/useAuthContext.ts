d12abe21a151b72319459cb7584a7d83a445673c
k-logic24 <49090551+k-logic24@users.noreply.github.com>
2022-04-19 20:39:05 +0900

    context hook修正

--- src/hooks/useAuthContext.ts
+++ src/hooks/useAuthContext.ts
@@ -3,9 +3,10 @@ import React, { useContext } from 'react'
 import { AuthContext, AuthContextProps } from '@/provider/AuthProvider'
 
 const useAuthContext = () => {
-  if (!AuthContext) throw new Error('please check context')
+  const authContext = useContext<AuthContextProps>(AuthContext)
+  if (!authContext) throw new Error('please check context')
 
-  return useContext<AuthContextProps>(AuthContext)
+  return authContext
 }
 
 export default useAuthContext
